3833f4e644b2b68cbaf286865cf1751c432cf568
AlbertoRo <albertorori@yahoo.es>
2019-11-24 10:37:31 +0100

    all
    Signed-off-by: AlbertoRo <albertorori@yahoo.es>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+
+
+
+
+function App() {
+    return (
+        <div>hola</div>
+
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
