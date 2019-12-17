6ea5aefbf7e4c44239dcb98ec9c6d7dc812c7199
AlbertoRo <albertorori@yahoo.es>
2019-12-17 22:24:39 +0100

    trabajando en carrito

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { useCart } from "../shared/context/cart-context";
 import { Link } from "react-router-dom";
 
 function Cart() {
+    const [order, setOrder] = useState(false);
     const {
         cart,
         totalPrice,
@@ -16,18 +17,24 @@ function Cart() {
 
     return (
         <React.Fragment>
+            <header className="home">
+                <h1>Aldach Has</h1>
+                <button className="btn call">Ayuda</button>
+                <button className="product-menu"><Link to={`/products`}>volver</Link></button>
+            </header>
             <ul>
                 {cart.map(item => (
                     <li key={item.id}>
-                        <p>
-                            <Link to={`/product/${item.id}`}>{`item.name (x${
-                                item.quantity
-                                })`}</Link>
-                            <span>{`$${item.price} - `}</span>
+                        <section>
+                            <Link to={`/product/${item.id}`}>{`${item.name} `}</Link>
+                            <p>{item.description}</p>
+                            <p> Precio Unidad:{`${item.price}€`}</p>
+                            <p>Cantidad: {item.quantity}</p>
                             <button
                                 onClick={e => {
                                     e.preventDefault();
                                     addItemToCart(item);
+                                    setOrder(false)
                                 }}
                             >
                                 +
@@ -36,6 +43,7 @@ function Cart() {
                                 onClick={e => {
                                     e.preventDefault();
                                     removeItemFromCart(item);
+                                    setOrder(false)
                                 }}
                             >
                                 -
@@ -44,19 +52,28 @@ function Cart() {
                                 onClick={e => {
                                     e.preventDefault();
                                     removeItem(item.id);
+                                    setOrder(false)
                                 }}
                             >
-                                Remove
+                                Elimiar Producto
                             </button>
-                        </p>
+                        </section>
                     </li>
                 ))}
             </ul>
-            <p>Total price = {`$${totalPrice}`}</p>
-            {totalItems > 0 && (
-                <button onClick={() => history.push("/confirmation")}>Comprar</button>
-            )}
-        </React.Fragment>
+            <p>IVA = {`${Number((totalPrice / 1.21).toFixed(2))}€`}</p>
+            <p>Total = {`${Number(totalPrice.toFixed(2))}€`}</p>
+            {
+                totalItems > 0 && !order && (
+                    <button onClick={() => setOrder(true)}>Confirmar Pedido</button>
+                )
+            }
+            {
+                order && (
+                    <button onClick={() => history.push("/confirmation")}>Pagar con tarjeta</button>
+                )
+            }
+        </React.Fragment >
     );
 }
 
